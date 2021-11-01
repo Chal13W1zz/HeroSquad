2f5f360c94a9fc52e8b9aef36c630aa4bd74eb5f
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-01 14:38:38 +0300

    dao connection success

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -1,2 +1,48 @@
-package dao;public class Sql2oHeroDao {
+package dao;
+
+import models.Hero;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oHeroDao implements HeroDao {
+    private final Sql2o sql2o;
+
+    public Sql2oHeroDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public List<Hero> getAllHeros() {
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM heros")
+                    .executeAndFetch(Hero.class);
+        }
+    }
+
+    @Override
+    public void addHero(Hero hero) {
+        String sql = "INSERT INTO heros (heroName,heroAge,heroPower,heroWeakness,avatarUrl) VALUES (:heroName,:heroAge,:heroPower,:heroWeakness,:avatarUrl)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(hero)
+                    .executeUpdate()
+                    .getKey();
+            hero.setId(id);
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+
+    }
+
+    @Override
+    public Hero findHeroById(int id) {
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM heros WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Hero.class);
+        }
+    }
 }
