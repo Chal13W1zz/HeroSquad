34c142a5a0a208df574e86d84aec50268d80f63e
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-01 12:39:00 +0300

    create a database template

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -2,9 +2,10 @@ SET MODE PostgresSQL;
 
 CREATE TABLE IF NOT EXISTS squads(
     id int PRIMARY KEY auto_increment,
-    squad_name VARCHAR,
-    squad_cause VARCHAR,
-    max_size int,
-    squad_full BOOLEAN
+    hero_name VARCHAR,
+    hero_age INT,
+    hero_power VARCHAR,
+    hero_weakness VARCHAR,
+    hero_avatar VARCHAR
 
 );
