e83685f9ac0c792ddd272724c2fa5d6ae0c544f9
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-01 10:28:29 +0300

    add is squad full set to false after instantiation test

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -13,15 +13,27 @@ class SquadTest {
     }
 
     @Test
-    public void NewSquadObjectGetsCorrectlyCreated_true(){
+    public void NewSquadObjectGetsCorrectlyCreated_true() throws Exception {
         Squad squad = setupNewSquad();
         assertNotNull(squad);
+    }
+
+    @Test
+    public void SquadInstantiatesWithNameAndCause_true() throws Exception {
+        Squad squad = setupNewSquad();
+        assertTrue(squad.getSquadName() != null && squad.getSquadCause() != null );
+    }
+
+    @Test
+    public void isSquadFullIsFalseAfterInstantiation() throws Exception{
+        Squad squad = setupNewSquad();
+        assertFalse(squad.isSquadFull());
     }
 
 
     //helper methods
     public  Squad setupNewSquad(){
-        return new Squad("Avengers","Fight Thanos",4);
+        return new Squad("Avengers","Fight Thanos",2);
     }
 
     @AfterEach
