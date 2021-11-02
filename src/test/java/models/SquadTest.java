20ec51a116ac328704e35c1443cb4d396ccf3be2
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-02 15:32:33 +0300

    add squad object tests

---
+++ src/test/java/models/SquadTest.java
@@ -0,0 +1,40 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getSquadName() {
+    }
+
+    @Test
+    void setSquadName() {
+    }
+
+    @Test
+    void getSquadCause() {
+    }
+
+    @Test
+    void setSquadCause() {
+    }
+
+    @Test
+    void getMaxSize() {
+    }
+
+    @Test
+    void setMaxSize() {
+    }
+}
